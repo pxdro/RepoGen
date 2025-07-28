482ad2198bdc9ec2c8771dcf6578cb6aa0c3a0be
Drope <pedrukes.rocha@gmail.com>
2025-07-28 15:53:50 -0300

    Folder name correction (Logs to LogContents)

---
+++ RepoGen/Interface/EntitiesSlices/LogContents/LogsQueryHandler.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RepoGen.Contexts;
+using RepoGen.Entities;
+
+namespace RepoGen.Interface.EntitiesSlices.Logs
+{
+    // Query
+    public record LogsQuery : IRequest<List<LogContent>>;
+
+    // Handler
+    public class LogsQueryHandler(AppDbContext context) : IRequestHandler<LogsQuery, List<LogContent>>
+    {
+        private readonly AppDbContext _context = context;
+
+        public async Task<List<LogContent>> Handle(LogsQuery request, CancellationToken cancellationToken)
+        {
+            return await _context.Logs.AsNoTracking().ToListAsync(cancellationToken);
+        }
+    }
+}
